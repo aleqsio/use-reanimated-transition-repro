9835fa124fefd4e165f6e701499eee9d2a8008d4
aleqsio <mikucki@gmail.com>
2022-05-12 12:51:52 +0200

    Add basic reproduction

--- App.tsx
+++ App.tsx
@@ -1,22 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import {
+  ReanimatedScreenProvider,
+  useReanimatedTransitionProgress,
+} from "react-native-screens/reanimated";
+import Animated, {
+  useAnimatedStyle,
+  useDerivedValue,
+} from "react-native-reanimated";
+import { Button } from "react-native";
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  return (
+    <SafeAreaProvider>
+      <Navigation />
+      <StatusBar />
+    </SafeAreaProvider>
+  );
+}
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <ReanimatedScreenProvider>
+        <RootNavigator />
+      </ReanimatedScreenProvider>
+    </NavigationContainer>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Root" component={RootScreen} />
+      <Stack.Screen name="Second" component={SecondScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function RootScreen() {
+  const { navigate } = useNavigation();
+  const reaProgress = useReanimatedTransitionProgress();
+  const sv = useDerivedValue(
+    () =>
+      (reaProgress.progress.value < 0.5
+        ? reaProgress.progress.value * 50
+        : (1 - reaProgress.progress.value) * 50) + 50
+  );
+  const reaStyle = useAnimatedStyle(() => {
+    return {
+      width: sv.value,
+      height: sv.value,
+      backgroundColor: "blue",
+    };
+  });
+
+  return (
+    <>
+      <Animated.View style={reaStyle} />
+      <Button title="Navigate to second screen" onPress={() => navigate("Second")} />
+    </>
+  );
+}
+function SecondScreen() {
+  return <></>;
 }
